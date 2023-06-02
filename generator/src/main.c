9d01e8a0b8c488bcf5b2394b95d7d19f2d4a544b
Denis <denis.hoornaert@tum.de>
2023-06-02 11:43:41 +0200

    rt-bench core: main: remove seemingly useless log of sched_deadline attr

--- generator/src/main.c
+++ generator/src/main.c
@@ -145,13 +145,6 @@ static int set_sched_fifo_prio(unsigned int prio)
 		return ret;
 	}
 
-	elogf(LOG_LEVEL_INFO,
-	      "\nsize: %u, policy: %u, flags: %llu, prio: %u"
-	      "\nT: %llu, D: %llu, P: %llu\n",
-	      attr.size, attr.sched_policy, attr.sched_flags,
-	      attr.sched_priority, attr.sched_runtime, attr.sched_deadline,
-	      attr.sched_period);
-
 	return ret;
 }
 
