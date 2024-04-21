b29279a636db60afe6e2b12985249a011b7b2020
Mattia Nicolella <mattianicolella@gmail.com>
2024-04-21 16:49:43 -0400

    fix(generator): fix broken recipe
    Signed-off-by: Mattia Nicolella <mattianicolella@gmail.com>

--- generator/rtbench.mk
+++ generator/rtbench.mk
@@ -152,7 +152,7 @@ endif
 init: staticx-check dlmalloc
 	@mkdir -p $(OBJECT)
 
-dlmalloc: $(OBJECT)/dlmalloc.o
+dlmalloc:
 	sed -E -i 's/#define MORECORE [^[:space:]]+/#define MORECORE sbrk/' $(SOURCE)/dlmalloc/source/dlmalloc.c
 	sed -i 's/#define MORECORE_CONTIGUOUS [01]/#define MORECORE_CONTIGUOUS 1/' $(SOURCE)/dlmalloc/source/dlmalloc.c
 	sed -i 's/#define HAVE_MORECORE [01]/#define HAVE_MORECORE 1/' $(SOURCE)/dlmalloc/source/dlmalloc.c
